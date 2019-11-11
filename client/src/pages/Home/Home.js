474fd6c7f23557a01d4cc3127469d7f9e1fc1032
Spencer Shadley <spencershadley@utexas.edu>
2019-11-10 17:19:23 -0800

    several UI updates

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -77,7 +77,14 @@ class Home extends Component {
                     <span className="text-white"><strong><span role="img" aria-label="badge">🎖 Current challenge</span></strong> {this.state.challenge}</span>
                 </nav> */}
                 <nav className="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
-                    <span className="text-white"><strong><span role="img" aria-label="badge">🎖 How can I earn points for the house cup?</span></strong> <a href="http://house-cup.netlify.com/">Click here to find out</a></span>
+                    <span className="text-white">
+                        <strong>
+                            <span role="img" aria-label="badge">🎖 How can I earn points for the house cup?</span>
+                        </strong>
+                        <a href="https://arronjlinton.github.io/HouseCupPoints/" target="_blank" rel='noreferrer noopener'>
+                            Click here to find out
+                        </a>
+                    </span>
                 </nav>
             </div>
         );
